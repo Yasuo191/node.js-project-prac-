55b37259dfab2675c24dd2d1ae4d716ff21fc073
Yasuo191 <24102865@st.phenikaa-uni.edu.vn>
2026-09-08 20:47:23 +0700

    feat: initial commit-initial package for project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express= require('express')
+const morgan = require('morgan')
+const {default:helmet}=require('helmet')
+const compression = require('compression')
+const app=express()
+
+
+// init middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+// init db
+app.get("/", (req, res, next)=>{
+    return res.status(200).json({
+        message: 'YoLo'
+    })
+})
+// init router
+
+// handling error
+
+module.exports=app
